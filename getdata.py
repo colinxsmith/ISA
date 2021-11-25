42d65ac32089ecfd260f192d12cc6b48c7125ff4
Colin <colinxsmith@gmail.com>
2021-11-25 21:18:06 +0000

    Tidy processing

--- getdata.py
+++ getdata.py
@@ -1,42 +1,28 @@
 import time
-
-print (time.time())
-print (time.ctime(1620601200))
-
 import json
-
-x =  '{ "name":"John", "age":30, "city":"New York"}'
-
-# parse x:
-y = json.loads(x)
-
-# the result is a Python dictionary:
-print(y["age"])
-
 from sys import stdin
-
 k=''
 for line in stdin.readlines():
     k=json.loads(line)
-
 alldata={}
 alltimes={}
 for stock in k.keys():
-    print('Stock %s'%(stock))
     alldata[stock]={}
     times=k[stock]['timestamp']
     close=k[stock]['close']
     for i in range(len(times)):
         alltimes[int(times[i])]=i
-        dd=time.ctime(times[i]).split()
-        print('%s %s\t%s'%(dd[1],dd[2],close[i]))
         alldata[stock][times[i]]=close[i]
 history=[int(i) for i in alltimes.keys()]
 history.sort()
+last=0
 for stock in k.keys():
     print (stock)
-    for hh in history:
+    for hhh in history:
+        hh=time.ctime(hhh).replace(' 00:00:00','')
         try:
-            print (hh,alldata[stock][hh])
+            if alldata[stock][hhh]==0.0:alldata[stock][hhh]=last
+            print ('%s %f' % (hh,alldata[stock][hhh]))
+            last=alldata[stock][hhh]
         except:
-            print ('no data')
+            print ('%s %f' % (hh,last))
