d86dac9ac55bbed0dac7a4a09771012bd3501b1b
Colin Smith <colinxsmith@gmail.com>
2022-02-21 19:55:19 +0000

    .

--- editnames.py
+++ editnames.py
@@ -1,8 +1,8 @@
 from os import system
 
-kk={'0P000023CA.L': 'Legal_&_General_US_Index_Trust', '0P000023C7.L': 'Legal_&_General_UK_Index_Trust', '0P000023CJ.L': 'Legal_&_General_Japan_Index_Tr', '0P00015E7M.L': 'Virgin_Global_Share_Fund', '0P000023HM.L': 'Legal_&_General_Pacific_Index_T', '0P000023NV.L': 'Legal_&_General_European_Index', '0P000023C5.L': 'L&G_MSCI_World_Socially_Respons', '0P00002059.L': 'Legal_&_General_Active_Sterling', '0P0000HN9B.L': 'JPM_Asia_Growth_Fund_B_-_Net_Ac'}
+namedict={'0P000023CA.L': 'Legal_&_General_US_Index_Trust', '0P000023C7.L': 'Legal_&_General_UK_Index_Trust', '0P000023CJ.L': 'Legal_&_General_Japan_Index_Tr', '0P00015E7M.L': 'Virgin_Global_Share_Fund', '0P000023HM.L': 'Legal_&_General_Pacific_Index_T', '0P000023NV.L': 'Legal_&_General_European_Index', '0P000023C5.L': 'L&G_MSCI_World_Socially_Respons', '0P00002059.L': 'Legal_&_General_Active_Sterling', '0P0000HN9B.L': 'JPM_Asia_Growth_Fund_B_-_Net_Ac'}
 
-for key in kk.keys():
-    comm='sed -i "s/%s/%s/i" GLdist'%(key,kk[key])
+for key in namedict.keys():
+    comm='sed -i "s/%s/%s/i" GLdist'%(key,namedict[key])
     print(comm)
     system(comm)
