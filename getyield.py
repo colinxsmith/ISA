1594f878cc5d0687fb48f5f38f23827e64cdfe52
Colin Smith <colinxsmith@gmail.com>
2023-12-25 17:29:42 +0000

    have a stocks file edit names in getyield.py

--- getyield.py
+++ getyield.py
@@ -2,10 +2,15 @@ import time
 import json
 import re
 from sys import stdin
+from os import system
 k=''
 fund=''
 for line in stdin.readlines():
     k=json.loads(line)
 for op in k.keys():
     fund=k[op]['result'][0]
+stock=fund['underlyingSymbol']
+longname=fund['quote']['longName']
+longname=longname.replace(' ','_')
+system('sed -i "s/%s/%s/" GLdist' % (stock,longname))
 print(fund['underlyingSymbol'],fund['quote']['longName'],fund['quote']['dividendYield'])
